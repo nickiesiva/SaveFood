78cd7636f26d1c9503d5de760ba919fe378a25ed
nickiesiva <nickiesiva@gmail.com>
2015-01-21 06:05:01 +0700

    Change name to Wretlinks

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Save your food"
+    assert_select "title", "Home | Write Your Links"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Save your food"
+    assert_select "title", "Help | Write Your Links"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Save your food"
+    assert_select "title", "About | Write Your Links"
   end
 
 end
