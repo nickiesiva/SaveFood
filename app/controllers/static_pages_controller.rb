13824bb5f12a789db5f068f661d4e395d4134c3e
nickiesiva <nickiesiva@gmail.com>
2015-01-22 13:43:41 +0700

    repair method 'feed' undefined

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     @postlink = current_user.postlinks.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page], per_page: 7)
+    if logged_in?
+      @postlink = Postlink.new
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 7)
+    end
   end
 
   def help
